bf97b7db6b13088f4c91336dd25bfc27b75cc3c1
Prestis <oresthsperrakhs@gmail.com>
2024-03-31 15:10:27 +0300

    Communication with mysql database added. Also implemented dynamic loading of products from database based on the product category tab.

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,101 @@
 const express = require('express');
 
+//var database = require('')
 const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const dbService = require('./dbService');
+const databaseService = require('./dbService');
+
+
+app.use(cors());
+app.use(express.json());
 app.use(express.static('public'));
 
-app.listen(3000,() => {
-    console.log("App listening on port 3000")
+app.listen(process.env.PORT,() => {
+    
+    console.log("App listening on port " + process.env.PORT)
+})
+
+
+//Read data from back-end  
+app.get('/getAll',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
+})
+app.get('/getCases',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getCasesData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
+})
+app.get('/getCPU',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getCPUData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
+})
+app.get('/getGPU',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getGPUData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
+})
+app.get('/getMotherboard',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getMotherboardData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
+})
+app.get('/getPSU',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getPSUData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
+})
+app.get('/getRAM',(request,response) =>{
+    const db = databaseService.getDbServiceInstance();
+
+    const result = db.getRAMData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    //console.log('test');
 })
